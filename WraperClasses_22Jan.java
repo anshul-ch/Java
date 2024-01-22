179963c6be9e8229d92c1ffc383a8d2b2277ad00
anshul-ch <anshul.chaudhary160603@gmail.com>
2024-01-22 11:26:22 +0530

    Wrapper Classes ( type casting)

---
+++ WraperClasses_22Jan.java
@@ -0,0 +1,7 @@
+public class WraperClasses_22Jan {
+    public static void main(String[] args) {
+        // Wrapper classes are used to convert primitive data types into objects and vice versa
+        // Primitive data types are: byte, short, int, long, float, double, boolean, char
+        // Wrapper classes are: Byte, Short, Integer, Long, Float, Double, Boolean, Character
+    }
+}
