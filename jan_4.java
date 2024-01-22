0baa6daa060be5a213402fe0c7f8c6ad32a7dd55
anshul-ch <anshul.chaudhary160603@gmail.com>
2024-01-22 11:26:28 +0530

    Wrapper Classes ( type casting)

--- jan_4.java
+++ jan_4.java
@@ -13,6 +13,7 @@ public class jan_4 {
         }
         else if(num1 < num2){
             System.out.println("num1 is less than num2");
+
         }
         else{
             System.out.println("num1 is equal to num2");
